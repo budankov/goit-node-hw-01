711a4fcc7fd990a44c8efa17a06f2c978fbcd1d4
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-03-25 15:01:01 +0200

    Update

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const contacts = require("./db/contacts.js");
+const { program } = require("commander");
+
+const contacts = require("./bd/contacts.js");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
+      console.table(allContacts);
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      console.table(contactById);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      console.table(deleteContact);
   }
 };
 
-invokeAction({ action: "list" });
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+console.log(argv);
+invokeAction(argv);
